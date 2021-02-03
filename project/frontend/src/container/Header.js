b3b920146c9b9c68e7396b58775f6682fb12dbd1
shreyas710 <shreyasnk5321@gmail.com>
2021-02-03 12:59:48 +0530

    sidebar

--- project/frontend/src/container/Header.js
+++ project/frontend/src/container/Header.js
@@ -3,14 +3,14 @@ import React, { useState, useContext } from "react";
 import "./Header.css";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import SearchIcon from "@material-ui/icons/Search";
-import { IconContext } from 'react-icons';
-import { Link } from 'react-router-dom';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Fade from '@material-ui/core/Fade';
+import { IconContext } from "react-icons";
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Fade from "@material-ui/core/Fade";
 
 const Header = () => {
   const [sidebar, setSidebar] = useState(false);
@@ -26,58 +26,70 @@ const Header = () => {
     setAnchorEl(null);
   };
   return (
-      <>
-        <div className='navbar'>
-            <Link to='#' className='menu-bars'>
-            <FaIcons.FaBars onClick={showSidebar}  style={{display:"flex",justifyContent:"flex-start"}}/>
+    <>
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <FaIcons.FaBars onClick={showSidebar} />
+        </Link>
+        <form style={{ marginRight: "auto", marginLeft: "auto" }}>
+          <input type="text" placeholder="Search a product..." />
+          <button>
+            <SearchIcon />
+          </button>
+        </form>
+
+        {/* <button  style={{display:"inline-block",justifyContent:"flex-end"}}><ShoppingCartIcon /></button> */}
+
+        <Button
+          style={{ justifyContent: "flex-end" }}
+          aria-controls="fade-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          Shopkeeper
+        </Button>
+        <Menu
+          id="fade-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Fade}
+        >
+          <MenuItem onClick={handleClose}>SignUp</MenuItem>
+          <MenuItem onClick={handleClose}>Login</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+        <Button
+          style={{ display: "flex", justifyContent: "flex-end" }}
+          aria-controls="fade-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          Customer
+        </Button>
+        <Menu
+          id="fade-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Fade}
+        >
+          <MenuItem onClick={handleClose}>SignUp</MenuItem>
+          <MenuItem onClick={handleClose}>Login</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </div>
+
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items" onClick={showSidebar}>
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+              <AiIcons.AiOutlineClose />
             </Link>
-            <form style={{marginRight:"auto",marginLeft:"auto"}}>
-                <input type="text" placeholder="Search a product..."/>
-                <button><SearchIcon /></button>
-            </form>
-
-            {/* <button  style={{display:"inline-block",justifyContent:"flex-end"}}><ShoppingCartIcon /></button> */}
-
-            <Button style={{justifyContent:"flex-end"}} aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
-                Shopkeeper
-            </Button>
-            <Menu
-                id="fade-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={open}
-                onClose={handleClose}
-                TransitionComponent={Fade}
-                >
-                <MenuItem onClick={handleClose}>SignUp</MenuItem>
-                <MenuItem onClick={handleClose}>Login</MenuItem>
-                <MenuItem onClick={handleClose}>Logout</MenuItem>
-            </Menu>
-            <Button style={{display:"flex",justifyContent:"flex-end"}} aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
-                Customer
-            </Button>
-            <Menu
-                id="fade-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={open}
-                onClose={handleClose}
-                TransitionComponent={Fade}
-                >
-                <MenuItem onClick={handleClose}>SignUp</MenuItem>
-                <MenuItem onClick={handleClose}>Login</MenuItem>
-                <MenuItem onClick={handleClose}>Logout</MenuItem>
-            </Menu>
-        </div>
-       
-        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-            <ul className='nav-menu-items' onClick={showSidebar}>
-            <li className='navbar-toggle'>
-                <Link to='#' className='menu-bars'>
-                <AiIcons.AiOutlineClose />
-                </Link>
-            </li>
-            {/* {SidebarData.map((item, index) => {
+          </li>
+          {/* {SidebarData.map((item, index) => {
                 return (
                 <li key={index} className={item.cName}>
                     <Link to={item.path}>
@@ -87,8 +99,8 @@ const Header = () => {
                 </li>
                 );
             })} */}
-            </ul>
-        </nav>
+        </ul>
+      </nav>
     </>
   );
 };
--- project/frontend/src/container/Sidebar.js
+++ project/frontend/src/container/Sidebar.js
@@ -2,22 +2,22 @@ import React, { useState, useContext } from "react";
 
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import SearchIcon from "@material-ui/icons/Search";
-import { IconContext } from 'react-icons';
-import { Link } from 'react-router-dom';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Fade from '@material-ui/core/Fade';
-import './Sidebar.css'
+import { IconContext } from "react-icons";
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Fade from "@material-ui/core/Fade";
+import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
     <>
-        <div className="sidebar">
-            <h1>Hello</h1>
-        </div>
+      <div className="sidebar">
+        <p align="center">List of items in cart</p>
+      </div>
     </>
   );
 };
