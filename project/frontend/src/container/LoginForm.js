bc689e679455a85e11945bd29a2eb0da63b91846
Aakash Patil <aakashpatil45321@gmail.com>
2021-02-05 17:16:01 +0530

    Customer SignUp

--- project/frontend/src/container/LoginForm.js
+++ project/frontend/src/container/LoginForm.js
@@ -7,6 +7,18 @@ import * as Yup from "yup";
 const LoginForm = () => (
     <Formik
       initialValues={{ email: "", password: "" }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           console.log("Logging in", values);
@@ -70,4 +82,4 @@ const LoginForm = () => (
     </Formik>
   );
   
-  export default ValidatedLoginForm;
+  export default LoginForm;
