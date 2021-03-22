b2ade6d7339f641715830d89aed45bf0325a4145
Kunal <kolambekm@gmail.com>
2021-03-22 23:15:11 +0530

    modal

--- project/frontend/src/Shopkeeper/pages/productCard.js
+++ project/frontend/src/Shopkeeper/pages/productCard.js
@@ -11,50 +11,43 @@ import {
 	Button,
 	CardLink,
 	CardGroup,
-	Modal,
 } from "reactstrap";
 
-// function MyVerticallyCenteredModal(props) {
-// 	return (
-// 		<Modal
-// 			{...props}
-// 			size="lg"
-// 			aria-labelledby="contained-modal-title-vcenter"
-// 			centered>
-// 			<Modal.Header closeButton>
-// 				<Modal.Title id="contained-modal-title-vcenter">
-// 					Modal heading
-// 				</Modal.Title>
-// 			</Modal.Header>
-// 			<Modal.Body>
-// 				<h4>Centered Modal</h4>
-// 				<p>
-// 					Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-// 					dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-// 					consectetur ac, vestibulum at eros.
-// 				</p>
-// 			</Modal.Body>
-// 			<Modal.Footer>
-// 				<Button onClick={props.onHide}>Close</Button>
-// 			</Modal.Footer>
-// 		</Modal>
-// 	);
-// }
+import {
+	Modal,
+} from 'react-bootstrap'
+
+
 
 const ProductCard = (props) => {
-	// const [modalShow, setModalShow] = useState(false);
+	const [modalShow, setModalShow] = useState(false);
 
 	const handleClick = () => {
-		// setModalShow(true);
-		alert("hello");
+		setModalShow(true);
+		// alert("hello");
 	};
 
 	return (
 		<>
-			{/* <MyVerticallyCenteredModal
-				show={modalShow}
-				onHide={() => setModalShow(false)}
-			/> */}
+		<Modal
+			size="lg"
+			aria-labelledby="contained-modal-title-vcenter"
+			centered
+			show={modalShow}
+			onHide={() => setModalShow(false)}>
+			<Modal.Header closeButton>
+				<Modal.Title id="contained-modal-title-vcenter">
+					{props.name}
+				</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<div></div>
+				
+			</Modal.Body>
+			<Modal.Footer>
+				<Button onClick={props.onHide}>Close</Button>
+			</Modal.Footer>
+		</Modal>
 			<Card style={{ width: "30%" }}>
 				<CardImg top width="100%" src={props.url} alt={props.name} />
 				<CardBody>
